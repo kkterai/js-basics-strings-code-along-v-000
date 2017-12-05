28cf3fadc866917375da0d6f244443fa27c53424
Kenlyn T <kkterai@gmail.com>
2017-12-05 08:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = "Learner";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+const shortGreeting = "Welcome, "
